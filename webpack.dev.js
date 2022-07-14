196dc10470302eb91683193b6c5067e71b097a06
Teemo Tebest <teemo.tebest@gmail.com>
2022-07-14 09:44:54 +0200

    Initialised project

---
+++ webpack.dev.js
@@ -0,0 +1,22 @@
+const { merge } = require('webpack-merge');
+const path = require('path');
+const common = require('./webpack.common.js');
+const ESLintPlugin = require('eslint-webpack-plugin');
+
+module.exports = merge(common, {
+  devtool: 'inline-source-map',
+  devServer: { 
+    hot: true,
+    static: path.resolve(__dirname, './public')
+  },
+  entry: {
+    app: './src/index.js'
+  },
+  mode: 'development',
+  plugins: [
+    new ESLintPlugin({
+      extensions: ['js', 'jsx'],
+      fix: true
+    })    
+  ]
+});
