8fed2ecb3a320fc71576c20be9d063c873a0c96c
Teemo Tebest <teemo.tebest@gmail.com>
2022-07-15 15:30:53 +0200

    Updated title

--- src/jsx/App.jsx
+++ src/jsx/App.jsx
@@ -117,7 +117,7 @@ function App() {
 
   return (
     <div className="app">
-      <h1>A pulse of the global crisis</h1>
+      <h1>Pulse of the global crisis</h1>
       <div className="dashboard_items">
         <DashBoardItem desc="Higher value means higher prices of food in supermarkets" idx="0" image={wheat} meta={meta_0} start={start_0} series={series_0.current} title="FAO Food Index" unit="%" value={value_0} />
         <DashBoardItem desc="Higher value means less governmental independence" idx="1" image={bill} meta={meta_1} start={start_1} series={series_1.current} title="Bond Spread Sovereign" unit="%" value={value_1} />
