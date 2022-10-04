2719c9d31cb331dd4f7c4d0576897bcd2bf7bfac
Teemo Tebest <teemo.tebest@gmail.com>
2022-10-04 18:21:10 +0200

    Cleaned code

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 
 import App from './jsx/App.jsx';
-
 import AppMini from './jsx/AppMini.jsx';
 
 const dashboard = document.getElementById('app-root-2022-ukraine_brief_3_dashboard');
